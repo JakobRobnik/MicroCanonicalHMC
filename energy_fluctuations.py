627ef9925042f94a7976b292c15b571d683e70b5
jakob.robnik <jakob.robnik@gmail.com>
2022-12-13 14:52:14 +0100

    translating moving median to jax

--- energy_fluctuations.py
+++ energy_fluctuations.py
@@ -13,6 +13,61 @@ import german_credit
 import jax
 import jax.numpy as jnp
 
+tab_colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+
+
+def moving_variance(x, w, width):
+    """smoothing by a moving average method:
+        smooth[i] = average(x[i - half_width : i + half_width + 1])
+    """
+
+    xsq = jnp.square(x)
+
+    def move_by_one(state, useless):
+        W, F1, F2, add, drop = state
+        W_bare = W - w[drop]
+        W_new = W_bare + w[add]
+        x1 = (W * F1 + w[add] * x[add] - w[drop] * x[drop]) / W_new
+        x2 = (W * F2 + w[add] * xsq[add] - w[drop] * xsq[drop]) / W_new
+
+        var = x2 - jnp.square(x1)
+        return (W_new, F1, F2, add + 1, drop + 1), var
+
+    Wi = jnp.sum(w[:width])
+    F1i = jnp.average(x[:width], weights = w[:width])
+    F2i = jnp.average(xsq[:width], weights=w[:width])
+    var = jax.lax.scan(move_by_one, init = (Wi, F1i, F2i, width, 0), xs = None, length= len(x) - width)[1]
+
+    return var
+
+
+
+def variance_with_weights(x, w):
+
+    avg = np.average(x, weights= w)
+    return np.average(np.square(x - avg), weights= w)
+
+
+def energy_variance(target, L, eps):
+    burn_in, samples = 1000, 1000
+    width = 61
+    sampler = mchmc.Sampler(target, L, eps, integrator='LF', generalized=True)
+
+    Xall, Wall, Eall = sampler.parallel_sample(num_cores, burn_in + samples, monitor_energy=True, num_cores= num_cores)
+    var_chains= np.empty(num_cores)
+    fullvar_chains= np.empty(num_cores)
+
+    for chain in range(num_cores):
+        X, W, E = Xall[chain, burn_in:, :], Wall[chain, burn_in:], Eall[chain, burn_in:]
+        var_t = moving_variance(E, W, 61)
+        plt.plot(var_t)
+        var_chains[chain] = np.median(var_t)
+        fullvar_chains[chain] = variance_with_weights(E, W)
+    plt.show()
+    varE = np.median(var_chains) / target.d
+    fullvarE = np.median(fullvar_chains) / target.d
+
+    return varE, fullvarE
 
 
 
@@ -21,45 +76,36 @@ def benchmarks():
     # targets
     names = ['Ill-Conditioned', 'Bi-Modal', 'Rosenbrock', "Neal's Funnel", 'German Credit', 'Stochastic Volatility']
     targets = [IllConditionedGaussian(100, 100.0), BiModal(d=50, mu1=0.0, mu2=8.0, sigma1=1.0, sigma2=1.0, f=0.2), Rosenbrock(d=36), Funnel(d=20), german_credit.Target(), StochasticVolatility()]
-    d = [tar.d for tar in targets]
 
     #optimal settings
     file = 'submission/Table generalized_LF_q=0.csv'
     results = pd.read_csv(file)
-    eps = np.array(results['eps'])
-    L = np.array(results['alpha']) * np.sqrt(d)
+    eps_all = np.array(results['eps'])
+    alpha_all = np.array(results['alpha'])
 
-    varE = np.empty(len(eps))
+    i_target = 0
+    target = targets[i_target]
+    eps_opt, L_opt = eps_all[i_target], alpha_all[i_target] * np.sqrt(target.d)
 
-    for i in range(len(names)):
-        print(names[i])
-        target = targets[i]
-        burn_in, samples = 1000, 1000
-        batch_size, num_batches = 50, 20
+    eps = eps_opt * np.logspace(-1, np.log10(2), 3)
+    var, fullvar = np.empty(len(eps)), np.empty(len(eps))
+    energy_variance(target, L_opt, eps_opt)
 
-        sampler = mchmc.Sampler(target, L[i], eps[i], integrator= 'LF', generalized=True)
+    for i in range(len(eps)):
+        print(i)
+        var[i], fullvar[i] = energy_variance(target, L_opt, eps[i])
 
-        X, W, E = sampler.parallel_sample(6, burn_in + num_batches * batch_size, monitor_energy=True, num_cores= 6)
+    plt.plot(eps / eps_opt, var, 'o:')
+    plt.plot(eps / eps_opt, fullvar, 'o:')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlabel(r'$\epsilon / \widehat{\epsilon}$')
+    plt.ylabel('Var[E] / d')
+    plt.show()
 
-        var = np.empty((6, num_batches))
-        for k in range(num_batches):
-            imin, imax= burn_in+ k * batch_size, burn_in + (k+1) * batch_size
-            e1 = np.average(E[:, imin:imax], weights=W[:, imin:imax], axis= 1)
-            e2 = np.average(np.square(E[:, imin:imax]), weights=W[:, imin:imax], axis= 1)
-            var[:, k] = (e2 - np.square(e1)) / target.d
 
-        Var = np.median(var, axis = 1)
-        varE[i] = np.median(Var)
-
-        plt.title(names[i])
-        for i in range(len(E)):
-            plt.plot(np.arange(len(E[0]))[1000:], E[i, 1000:], '.')
-        plt.savefig(names[i] + '.png')
-        plt.show()
-
-    results['energy variance'] = varE
-
-    results.to_csv('submission/Table generalized_LF_q=0_energy.csv', index = False)
+    #results['energy variance'] = varE
+    #results.to_csv('submission/Table generalized_LF_q=0_energy.csv', index = False)
 
 
 
@@ -134,6 +180,15 @@ def dimension_dependence():
 
 
 if __name__ == '__main__':
+
+    # from line_profiler import LineProfiler
+    #
+    # lprofiler = LineProfiler()
+    # lprofiler.add_function(energy_variance)  # add functions that you would like to time
+    #
+    # lp_wrapper = lprofiler(benchmarks)  # the main function which will run the program
+    # lp_wrapper()  # run program
+    # lprofiler.print_stats()  # print times
 
     benchmarks()
     #dimension_dependence()
