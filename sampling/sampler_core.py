f0b9fbdde6d20935cc6e84360398f324da7ffeae
jakob.robnik <jakob.robnik@gmail.com>
2023-03-08 09:35:13 +0100

    updated sampler_core

--- sampling/sampler_core.py
+++ sampling/sampler_core.py
@@ -1,9 +1,6 @@
 import jax
 import jax.numpy as jnp
 
-jax.config.update('jax_enable_x64', True)
-
-
 lambda_c = 0.1931833275037836 #critical value of the lambda parameter for the minimal norm integrator
 
 
@@ -61,65 +58,77 @@ class Sampler:
 
     def update_momentum(self, eps, g, u):
         """The momentum updating map of the ESH dynamics (see https://arxiv.org/pdf/2111.02434.pdf)"""
+
         g_norm = jnp.sqrt(jnp.sum(jnp.square(g)))
         e = - g / g_norm
         ue = jnp.dot(u, e)
-        sh = jnp.sinh(eps * g_norm / self.Target.d)
-        ch = jnp.cosh(eps * g_norm / self.Target.d)
+        sh = jnp.sinh(eps * g_norm / (self.Target.d-1))
+        ch = jnp.cosh(eps * g_norm / (self.Target.d-1))
+        th = jnp.tanh(eps * g_norm / (self.Target.d-1))
+        delta_r = jnp.log(ch) + jnp.log1p(ue * th)
 
-        return (u + e * (sh + ue * (ch - 1))) / (ch + ue * sh)
+        return (u + e * (sh + ue * (ch - 1))) / (ch + ue * sh), delta_r
 
 
-    def leapfrog(self, x, g, u):
+    def leapfrog(self, x, u, g):
         """leapfrog"""
 
         #half step in momentum
-        uu = self.update_momentum(self.eps * 0.5, g, u)
+        uu, r1 = self.update_momentum(self.eps * 0.5, g, u)
 
         #full step in x
         xx = x + self.eps * uu
-        gg = self.Target.grad_nlogp(xx) * self.Target.d / (self.Target.d - 1)
+        l, gg = self.Target.grad_nlogp(xx)
 
         #half step in momentum
-        uu = self.update_momentum(self.eps * 0.5, gg, uu)
+        uu, r2 = self.update_momentum(self.eps * 0.5, gg, uu)
 
-        return xx, gg, uu
+        kinetic = (r1+r2) * (self.Target.d-1)
+
+        return xx, uu, l, gg, kinetic
 
 
 
-    def minimal_norm(self, x, g, u):
-        """Integrator from https://arxiv.org/pdf/hep-lat/0505020.pdf, see Equation 20."""
+    def minimal_norm(self, x, u, g):
+        """Integrator from https://arxiv.org/pdf/hep-lat/0505020.pdf, see Equation 20.
+            V T V T V scheme
+        """
 
-        # V T V T V
+        #V (momentum update)
+        uu, r1 = self.update_momentum(self.eps * lambda_c, g, u)
 
-        uu = self.update_momentum(self.eps * lambda_c, g, u)
+        #T (postion update)
+        xx = x + 0.5 * self.eps * uu
+        ll, gg = self.Target.grad_nlogp(xx)
 
-        xx = x + self.eps * 0.5 * uu
-        gg = self.Target.grad_nlogp(xx) * self.Target.d / (self.Target.d - 1)
+        #V (momentum update)
+        uu, r2 = self.update_momentum(self.eps * (1 - 2 * lambda_c), gg * self.sigma, uu)
 
-        uu = self.update_momentum(self.eps * (1 - 2 * lambda_c), gg, uu)
+        #T (postion update)
+        xx = xx + 0.5 * self.eps * uu
+        ll, gg = self.Target.grad_nlogp(xx)
 
-        xx = xx + self.eps * 0.5 * uu
-        gg = self.Target.grad_nlogp(xx) * self.Target.d / (self.Target.d - 1)
+        #V (momentum update)
+        uu, r3 = self.update_momentum(self.eps * lambda_c, gg, uu)
 
-        uu = self.update_momentum(self.eps * lambda_c, gg, uu)
+        #kinetic energy change
+        kinetic = (r1 + r2 + r3) * (self.Target.d-1)
 
-        return xx, gg, uu
-
+        return xx, uu, ll, gg, kinetic
 
 
     def dynamics(self, state):
         """One step of the Langevin-like dynamics."""
 
-        x, u, g, key, time = state
+        x, u, g, key = state
 
         # Hamiltonian step
-        xx, gg, uu = self.hamiltonian_dynamics(x, g, u)
+        xx, uu, ll, gg, kinetic = self.hamiltonian_dynamics(x, u, g)
 
         # add noise to the momentum direction
         uu, key = self.partially_refresh_momentum(uu, key)
 
-        return xx, uu, gg, key, 0.0
+        return xx, uu, ll, gg, kinetic, key
 
 
 
@@ -142,12 +151,12 @@ class Sampler:
         else: #initial x is given
             x = x_initial
 
-        g = self.Target.grad_nlogp(x) * self.Target.d / (self.Target.d - 1)
+        l, g = self.Target.grad_nlogp(x)
 
         u, key = self.random_unit_vector(key) #random initial direction
         #u = - g / jnp.sqrt(jnp.sum(jnp.square(g))) #initialize momentum in the direction of the gradient of log p
 
-        return x, u, g, key
+        return x, u, l, g, key
 
 
 
@@ -165,14 +174,39 @@ class Sampler:
         def step(state, useless):
             """Tracks transform(x) as a function of number of iterations"""
 
-            x, u, g, key, time = self.dynamics(state)
+            x, u, l, g, E, key = state
+            xx, uu, ll, gg, kinetic_change, key = self.dynamics(x, u, g, key)
+            EE = E + kinetic_change + ll - l
+            return (xx, uu, ll, gg, EE, key), (self.Target.transform(xx), ll, EE)
 
-            return (x, u, g, key, time), self.Target.transform(x)
-
-
-        x, u, g, key = self.get_initial_conditions(x_initial, random_key)
-
+        x, u, l, g, key = self.get_initial_conditions(x_initial, random_key)
 
         ### do sampling ###
+        X, L, E = jax.lax.scan(step, init=(x, u, l, g, 0.0, key), xs=None, length=num_steps)[1]
 
-        return jax.lax.scan(step, init=(x, u, g, key, 0.0), xs=None, length=num_steps)[1]
+        #reutrn X, E, burn_in_ending(L)         ### automatically detects the end of burn-in ###
+        return X, E
+
+
+
+### these two are only needed for the end of burn-in detection:
+
+def find_crossing(array, cutoff):
+    """find the smallest M such that array[m] < cutoff for all m > M"""
+
+    def step(carry, element):
+        """carry = (, 1 if (array[i] > cutoff for all i < current index) else 0"""
+        above_threshold = element > cutoff
+        never_been_below = carry[1] * above_threshold  #1 if (array[i] > cutoff for all i < current index) else 0
+        return (carry[0] + never_been_below, never_been_below), above_threshold
+
+    state, track = jax.lax.scan(step, init=(0, 1), xs=array, length=len(array))
+
+    return state[0]
+    #return jnp.sum(track) #total number of indices for which array[m] < cutoff
+
+
+
+def burn_in_ending(loss):
+    loss_avg = jnp.median(loss[len(loss) // 2:])
+    return 2 * find_crossing(loss - loss_avg, 0.0)  # we add a safety factor of 2
