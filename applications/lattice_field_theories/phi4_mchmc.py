bba3953b6ee62c992cf87fccaadab8ad8235b225
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2023-03-28 12:22:02 -0700

    fixing some bugs in phi^4 MCHMC, checkpoint before adding annealing

--- applications/lattice_field_theories/phi4_mchmc.py
+++ applications/lattice_field_theories/phi4_mchmc.py
@@ -20,7 +20,6 @@ dir = os.path.dirname(os.path.realpath(__file__))
 #params_critical_line = pd.read_csv(dir + '/theories/phi4_parameters.csv')
 
 
-
 def parallel_run(function, values):
     parallel_function= jax.pmap(jax.vmap(function))
     results = jnp.array(parallel_function(values.reshape(num_cores, len(values) // num_cores)))
@@ -37,7 +36,7 @@ def get_params(side):
 def ground_truth():
     
     index = 3
-    side = ([8, 16, 32, 64, 128])[index]
+    side = ([8, 16, 32, 64])[index]
 
     lam = phi4.unreduce_lam(phi4.reduced_lam, side)
     keys = jax.random.split(jax.random.PRNGKey(42), 8)  # we run 8 independent chains
@@ -45,7 +44,7 @@ def ground_truth():
     folder = dir + '/phi4results/mchmc/ground_truth/chi/'
     
     def f(i_lam, i_repeat):
-        return sample_chi(side, lam[i_lam], 1000000, keys[i_repeat], 1.0, 0.15)
+        return sample_chi(side, lam[i_lam], 5000000, keys[i_repeat], 0.6, 0.05)
 
 
     
@@ -57,19 +56,20 @@ def ground_truth():
 
     
 
-def sample(side, lam, num_samples, key, alpha, beta):
+# def sample(side, lam, num_samples, key, alpha, beta, integrator):
     
-    target = phi4.Theory(side, lam)
-    sampler = Sampler(target, L=jnp.sqrt(target.d) * alpha, eps= jnp.sqrt(target.d) * beta, integrator='MN')
+#     target = phi4.Theory(side, lam)
+#     target.transform = lambda x: x
+#     sampler = Sampler(target, L=jnp.sqrt(target.d) * alpha, eps= jnp.sqrt(target.d) * beta, integrator= integrator)
 
-    phi, E, burnin = sampler.sample(num_samples, output = 'full')
-    burnin = 1000
-
-    phi_reshaped = phi.reshape(num_samples, target.L, target.L)[burnin:]
+#     phi = sampler.sample(num_samples, tune = 'none', random_key = key)
+#     burnin = 1000
+#     phi_reshaped = phi.reshape(num_samples, target.L, target.L)[burnin:]
     
-    P = jax.vmap(target.psd)(phi_reshaped)
-    Pchain = jnp.cumsum(P, axis= 0) / jnp.arange(1, 1 + num_samples-burnin)[:, None, None]
-    return Pchain
+#     P = jax.vmap(target.psd)(phi_reshaped)
+#     Pchain = jnp.cumsum(P, axis= 0) / jnp.arange(1, 1 + num_samples-burnin)[:, None, None]
+#     return Pchain
+
 
 
 def sample_chi(side, lam, num_samples, key, alpha, beta):
@@ -77,143 +77,149 @@ def sample_chi(side, lam, num_samples, key, alpha, beta):
     target = phi4.Theory(side, lam)
     sampler = Sampler(target, L=jnp.sqrt(target.d) * alpha, eps= jnp.sqrt(target.d) * beta, integrator='MN')
 
-    phibar, burnin = sampler.sample(num_samples, output = 'normal')
+    phibar = sampler.sample(num_samples)
     burnin = 1000
     return phi4.reduce_chi(target.susceptibility2(phibar[burnin:, 0]), side)
     
     
 
+# def grid_search():
+    
+#     index = 1
+#     side = ([8, 16, 32, 64])[index]
+#     folder = dir + '/phi4results/mchmc/ess/psd/grid_search/'
+    
+#     integrator = 'MN'
+#     grads_per_step = 2.0 if integrator == 'MN' else 1.0
+    
+#     num_samples= 2000
+#     burnin = 500
+
+#     lam = phi4.unreduce_lam(phi4.reduced_lam, side)
+#     PSD0 = jnp.array(np.median(np.load(dir + '/phi4results/nuts/ground_truth/psd/L' + str(side) + '.npy').reshape(len(lam), 8, side, side), axis =1))
+
+#     #We run multiple independent chains to average ESS over them. Each of the 4 GPUs simulatanously runs repeat1 chains for each lambda
+#     #This is repeated sequentially repeat2 times. In total we therefore get 4 * repeat1 * repeat2 chains.
+#     repeat1 = ([20, 10, 100, 100])[index]
+#     keys = jax.random.split(jax.random.PRNGKey(42), repeat1)
+
+
+#     def f(i_lam, i_repeat):
+#         PSD = jax.vmap(jax.vmap(lambda a, b: sample(side, lam[i_lam], num_samples, keys[i_repeat], a, b, integrator)))(Alpha.T, Beta.T)
+#         b2_sq = jnp.average(jnp.square(1 - (PSD / PSD0[None, None, i_lam, :, :])), axis=(-2, -1))  # shape = (n_samples,)
+#         return b2_sq
+    
+#     fvmap = lambda x, y: jax.pmap(jax.vmap(lambda xx: jax.vmap(f, (None, 0))(xx, y)))(x.reshape(4, 4))
+
+#     _b2_sq = fvmap(jnp.arange(len(lam)), jnp.arange(repeat1)).reshape(len(lam), repeat1, len(alpha), len(beta), num_samples-burnin)
+#     b2_sq = jnp.average(_b2_sq, axis=1)
+#     num_steps = np.argmax(b2_sq < 0.01, axis = -1)
+        
+#     ess = (200.0 / (grads_per_step * num_steps)) * (num_steps != 0)
+#     np.save(folder + 'L' + str(side) + '.npy', ess)
+
+
+def load_data(index):
+    side = ([8, 16, 32, 64])[index]
+    integrator = 'MN'
+    lam = phi4.unreduce_lam(phi4.reduced_lam, side)
+    PSD0 = jnp.array(np.median(np.load(dir + '/phi4results/nuts/ground_truth/psd/L' + str(side) + '.npy').reshape(len(lam), 8, side, side), axis =1))
+    
+    return side, integrator, lam, PSD0
+
+
+def sample(lamb, side, tune, alpha, beta, integrator, num_samples, remove, chains, PSD0):
+    
+    grads_per_step = 2.0 if integrator == 'MN' else 1.0
+    target = phi4.Theory(side, lamb)
+    target.transform = lambda x: x
+    sampler = Sampler(target, alpha * jnp.sqrt(target.d), beta * jnp.sqrt(target.d), integrator= integrator)
+    sampler.frac_num1 = 0.1
+    sampler.frac_num1 = 0.1
+    sampler.varEwanted = 3e-2#1e-3 #targeted energy variance Var[E]/d
+
+    phi1, E, L, eps = sampler.sample(num_samples, chains, tune = tune, output = 'detailed')
+
+    vare= jnp.average(jnp.square(E[:, 1:]-E[:, :-1]), axis = 1)/target.d
+    varavg, varstd = jnp.average(vare), jnp.std(vare)
+    Lavg, Lstd, epsavg, epsstd = jnp.average(L), jnp.std(L), jnp.average(eps), jnp.std(eps)
+
+    phi = phi1.reshape(chains, num_samples, side, side)[:, remove:, :, :]
+    P = jax.vmap(jax.vmap(target.psd))(phi)
+    PSD = jnp.cumsum(P, axis= 1) / jnp.arange(1, 1 + num_samples-remove)[None, :, None, None] #shape = (chains, samples, L, L)
+
+    b2_sq = jnp.average(jnp.square(1 - (PSD / PSD0[None, None, :, :])), axis=(0, 2, 3))  # shape = (samples,)
+    num_steps = jnp.argmax(b2_sq < 0.01)
+    return (200.0 / (num_steps * grads_per_step)) * (num_steps != 0), Lavg, Lstd, epsavg, epsstd, varavg, varstd
+
+    
+def tuning_free():
+    
+    index = 2
+    side, integrator, lam, PSD0 = load_data(index)
+    
+    tune = 'cheap'
+    
+    chains = ([100, 100, 12, 4])[index]
+
+    if tune == 'cheap':
+        num_samples= 5000
+        remove= 2000
+    else:
+        num_samples = 3000
+        remove = 1000
+    
+    if index < 2:
+        data_tuple = jax.vmap(jax.vmap(lambda i_lam: sample(lam[i_lam], side, tune, 0.6, 0.3, integrator, num_samples, remove, chains, PSD0[i_lam])))(jnp.arange(16).reshape(4, 4))
+        data = np.array([np.concatenate(dd) for dd in data_tuple]).T
+        
+    else: #there is not enough space on the GPU, we do lambda sequentially
+        data = [sample(lam[i_lam], side, tune, 0.6, 0.3, integrator, num_samples, remove, chains, PSD0[i_lam]) for i_lam in range(16)]
+            
+    df = pd.DataFrame(data, columns = ['ESS', 'L', 'L err', 'eps', 'eps err', 'Var[E]/d', 'Var[E]/d err'])
+    df.to_csv(dir + '/phi4results/mchmc/ess/psd/autotune/' + tune + '_LL' + str(side) + '.csv', index =False)
+
+    
+
 def grid_search():
     
+    index = 3
+    side, integrator, lam, PSD0 = load_data(index)
+    
     alpha = jnp.logspace(jnp.log10(0.4), jnp.log10(3.0), 6)
-    beta = jnp.logspace(jnp.log10(0.1), jnp.log10(0.5), 6)
+    beta = jnp.logspace(jnp.log10(0.2), jnp.log10(0.5), 6)
     Alpha, Beta = jnp.meshgrid(alpha, beta)
-    index = 0
-    side = ([8, 16, 32, 64])[index]
-    folder = dir + '/phi4results/mchmc/ess/psd/'
+    tune = 'none'
     
-    num_samples= 10000
-    burnin = 1000
+    chains = ([100, 24, 12, 12])[index]
 
+    num_samples = 3000
+    remove = 1000
+    if index< 2:
+        data = [jax.vmap(jax.vmap(lambda a, b: sample(lam[i_lam], side, tune, a, b, integrator, num_samples, remove, chains, PSD0[i_lam])[0]))(Alpha.T, Beta.T) for i_lam in range(16)]
+    elif index == 2:
+        data = [[jax.vmap(lambda b: sample(lam[i_lam], side, tune, a, b, integrator, num_samples, remove, chains, PSD0[i_lam])[0])(beta) for a in alpha] for i_lam in range(16)]
+    else:
+        data = [[[sample(lam[i_lam], side, tune, a, b, integrator, num_samples, remove, chains, PSD0[i_lam])[0] for b in beta] for a in alpha] for i_lam in range(16)]
+        
+        
+    np.save(dir + '/phi4results/mchmc/ess/psd/grid_search/L' + str(side) + '.npy', data)
+    
+    
+def timing():
+    side = 2**12
     lam = phi4.unreduce_lam(phi4.reduced_lam, side)
-    PSD0 = jnp.array(np.median(np.load(dir + '/phi4results/hmc/ground_truth/psd/L' + str(side) + '.npy').reshape(len(lam), 8, side, side), axis =1))
-
-    #We run multiple independent chains to average ESS over them. Each of the 4 GPUs simulatanously runs repeat1 chains for each lambda
-    #This is repeated sequentially repeat2 times. In total we therefore get 4 * repeat1 * repeat2 chains.
-    repeat1 = ([100, 100, 1, 10])[index]
-    repeat2 = ([10, 10, 10, 100])[index]
-    keys = jax.random.split(jax.random.PRNGKey(42), repeat1*repeat2)
-
-
-    def f(i_lam, i_repeat):
-        PSD = jax.vmap(jax.vmap(lambda a, b: sample(side, lam[i_lam], num_samples, keys[i_repeat], a, b)))(Alpha.T, Beta.T)
-        b2_sq = jnp.average(jnp.square(1 - (PSD / PSD0[None, None, i_lam, :, :])), axis=(-2, -1))  # shape = (n_samples,)
-        return b2_sq
-    
-    fvmap = lambda x, y: jax.pmap(jax.vmap(lambda xx: jax.vmap(f, (None, 0))(xx, y)))(x.reshape(4, 4))
-
-    b2_sq = jnp.zeros((len(lam), num_samples-burnin, len(alpha), len(beta)))
-    
-
-    for r2 in range(repeat2):
-        _b2_sq = fvmap(jnp.arange(len(lam)), jnp.arange(r2*repeat1, (r2+1)*repeat1))
-        b2_sq += jnp.average(_b2_sq.reshape(len(lam), repeat1, num_samples-burnin, len(alpha), len(beta)), axis=1)
-
-    b2_sq /= repeat2
-
-    num_steps = jnp.argmax(b2_sq < 0.01, axis=1)
-    ess = (200.0 / (num_steps)) * (num_steps != 0)
-  
-    np.save(folder + 'L' + str(side) + '.npy', ess)
-
-
-# def grid_search2():
-    
-#     alpha = 1.0
-#     beta = jnp.logspace(0.1, 0.5, 6)
-#     index = 0
-#     side = ([8, 16, 32, 64])[index]
-#     folder = dir + '/phi4results/mchmc/ess/psd/'
-    
-#     num_samples= 10000
-#     burnin = 1000
-
-#     lam = phi4.unreduce_lam(phi4.reduced_lam, side)
-#     PSD0 = jnp.array(np.median(np.load(dir + '/phi4results/hmc/ground_truth/psd/L' + str(side) + '.npy').reshape(len(lam), 8, side, side), axis =1))
-
-#     #We run multiple independent chains to average ESS over them. Each of the 4 GPUs simulatanously runs repeat1 chains for each lambda
-#     #This is repeated sequentially repeat2 times. In total we therefore get 4 * repeat1 * repeat2 chains.
-#     repeat1 = ([1000, 1000, 1000, 1000])[index]
-#     repeat2 = ([1, 1, 1, 1])[index]
-#     keys = jax.random.split(jax.random.PRNGKey(42), repeat1*repeat2)
-
-#     def f(i_lam, i_repeat):
-#         PSD = jax.vmap(lambda b: sample(side, lam[i_lam], num_samples, keys[i_repeat], alpha, b))(beta)
-#         b2_sq = jnp.average(jnp.square(1 - (PSD / PSD0[None, i_lam, :, :])), axis=(-2, -1))  # shape = (n_samples,)
-#         return b2_sq
-
-#     fvmap = lambda x, y: jax.pmap(jax.vmap(lambda xx: jax.vmap(f, (None, 0))(xx, y)))(x.reshape(4, 4))
-
-#     b2_sq = jnp.zeros((len(lam), num_samples-burnin, len(beta)))
-    
-#     for r2 in range(repeat2):
-#         _b2_sq = fvmap(jnp.arange(len(lam)), jnp.arange(r2*repeat1, (r2+1)*repeat1))
-#         b2_sq += jnp.average(_b2_sq.reshape(16, repeat1, num_samples-burnin, len(beta)), axis=1)
-
-#     b2_sq /= repeat2
-
-#     num_steps = jnp.argmax(b2_sq < 0.01, axis=1)
-#     ess = (200.0 / (num_steps)) * (num_steps != 0)
-  
-#     np.save(folder + 'L' + str(side) + '.csv', ess)
-
-    
-# def compute_ess():
-    
-#     alpha = 1.0
-#     beta = 0.2
-#     index = 0
-#     side = ([8, 16, 32, 64])[index]
-#     folder = dir + '/phi4results/mchmc/ess/psd/'
-    
-#     num_samples= 10000
-#     burnin = 1000
-
-#     lam = phi4.unreduce_lam(phi4.reduced_lam, side)
-#     PSD0 = jnp.array(np.median(np.load(dir + '/phi4results/hmc/ground_truth/psd/L' + str(side) + '.npy').reshape(len(lam), 8, side, side), axis =1))
-
-#     #We run multiple independent chains to average ESS over them. Each of the 4 GPUs simulatanously runs repeat1 chains for each lambda
-#     #This is repeated sequentially repeat2 times. In total we therefore get 4 * repeat1 * repeat2 chains.
-#     repeat1 = ([1000, 1000, 1000, 1000])[index]
-#     repeat2 = ([1, 1, 1, 1])[index]
-#     keys = jax.random.split(jax.random.PRNGKey(42), repeat1*repeat2)
-
-#     def f(i_lam, i_repeat):
-#         PSD = sample(side, lam[i_lam], num_samples, keys[i_repeat], alpha, beta)
-#         b2_sq = jnp.average(jnp.square(1 - (PSD / PSD0[i_lam, :, :])), axis=(1, 2))  # shape = (n_samples,)
-#         return b2_sq
-
-#     fvmap = lambda x, y: jax.pmap(jax.vmap(lambda xx: jax.vmap(f, (None, 0))(xx, y)))(x.reshape(4, 4))
-
-#     b2_sq = jnp.zeros((len(lam), num_samples-burnin))
-    
-#     for r2 in range(repeat2):
-#         _b2_sq = fvmap(jnp.arange(len(lam)), jnp.arange(r2*repeat1, (r2+1)*repeat1))
-#         b2_sq += jnp.average(_b2_sq.reshape(16, repeat1, num_samples-burnin), axis=1)
-
-#     b2_sq /= repeat2
-
-#     num_steps = jnp.argmax(b2_sq < 0.01, axis=1)
-#     ess = (200.0 / (num_steps)) * (num_steps != 0)
-  
-#     np.save(folder + 'L' + str(side) + '_'+str(alpha)+'_'+str(beta)+'.csv', ess)
-
+    target = phi4.Theory(side, lam[-1])
+    sampler = Sampler(target, L=jnp.sqrt(target.d) * 0.6, eps= jnp.sqrt(target.d) * 0.3, integrator= 'MN')
+    phi = sampler.sample(250, tune = 'none', output = 'expectation')
     
     
-
 t0 = time.time()
 
-grid_search()#compute_ess()
+#timing()
+tuning_free()
+#grid_search()
+#compute_ess()
 #ground_truth()
 
 t1 = time.time()
--- applications/lattice_field_theories/theories/phi4.py
+++ applications/lattice_field_theories/theories/phi4.py
@@ -3,81 +3,31 @@ import jax.numpy as jnp
 import numpy as np
 
 
-class Lattice:
-    def __init__(self, L, d, BC='periodic'):
-        self.L = L
-        self.d = d
-        self.shape = [L]*d
-        self.Nsite = L**d
-        self.BC = BC
-
-    def move(self, idx, d, shift):
-        coord = self.index2coord(idx)
-        coord[d] += shift
-
-        if self.BC != 'periodic':
-            if (coord[d]>=self.L) or (coord[d]<0):
-                return None
-        #wrap around because of the PBC
-        if (coord[d]>=self.L): coord[d] -= self.L;
-        if (coord[d]<0): coord[d] += self.L;
-
-        return self.coord2index(coord)
-
-    def index2coord(self, idx):
-        coord = np.zeros(self.d, int)
-        for d in range(self.d):
-            coord[self.d-d-1] = idx%self.L;
-            idx /= self.L
-        return coord
-
-    def coord2index(self, coord): #(i, j) -> i * L + j
-        idx = coord[0]
-        for d in range(1, self.d):
-            idx *= self.L;
-            idx += coord[d]
-        return idx
-
-
-class Hypercube(Lattice):
-    def __init__(self, L, d, BC='periodic', laplace = False):
-        super(Hypercube, self).__init__(L, d, BC)
-        self.Adj = np.zeros((self.Nsite,self.Nsite))
-        for i in range(self.Nsite):
-            if laplace:
-                self.Adj[i, i] = -2.0 * self.d
-            for d in range(self.d):
-                j = self.move(i, d, 1)
-                if j is not None:
-                    self.Adj[i, j] = 1.0
-                    self.Adj[j, i] = 1.0
-
 
 class Theory:
     """Latice Phi^4 theory (MCHMC target class)"""
 
-    def __init__(self, L, lam, m_sq = -4):
+    def __init__(self, L, lam):
         """m^2 = -4, such that the diagonal terms cancel out"""
 
         self.d = L**2
         self.L = L
-
-        if m_sq == -4:
-            K = jnp.asarray(Hypercube(L, 2, 'periodic').Adj)
-            self.nlogp = lambda phi: - phi @ K @ phi + lam* jnp.sum(jnp.power(phi, 4))
-
-        else:
-            laplace = jnp.asarray(Hypercube(L, 2, 'periodic', laplace= True).Adj)
-            self.nlogp = lambda phi: - phi @ laplace @ phi + jnp.sum(m_sq * jnp.square(phi) + lam * jnp.power(phi, 4))
-
-
+        self.lam = lam
+        
         self.grad_nlogp = jax.value_and_grad(self.nlogp)
 
         self.transform = lambda phi: jnp.ones(1) * jnp.average(phi)
 
         self.prior_draw = lambda key: jax.random.normal(key, shape = (self.d, ), dtype = 'float64') #gaussian prior
-
-
+        
+    
+    def nlogp(self, x):
+        """action of the theory"""
+        phi = x.reshape(self.L, self.L)
+        action_density = self.lam*jnp.power(phi, 4) - phi*(jnp.roll(phi, -1, 0) + jnp.roll(phi, 1, 0) + jnp.roll(phi, -1, 1) + jnp.roll(phi, 1, 1))
+        return jnp.sum(action_density)
+        
+        
     #Observables
 
     def susceptibility1(self, phibar):
@@ -102,80 +52,25 @@ class Theory:
 
         return self.d * (phi_2 - jnp.square(phi_1))
 
+    
     def psd(self, phi):
         return jnp.square(jnp.abs(jnp.fft.fft2(phi))) / self.L ** 2
 
-    def greens_function0(self, phi):
-        """zero momentum 2-point Green's function, Equations 22 and 23 in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.100.034515"""
-        phi_lattice = np.reshape(phi, (self.side, self.side))
-        phi_t = np.sum(phi_lattice, axis=0)
-        phi_omega = np.fft.rfft(phi_t)
-        return np.fft.irfft(np.abs(np.square(phi_omega)), self.side)
+    
+#     def greens_function0(self, phi):
+#         """zero momentum 2-point Green's function, Equations 22 and 23 in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.100.034515"""
+#         phi_lattice = np.reshape(phi, (self.side, self.side))
+#         phi_t = np.sum(phi_lattice, axis=0)
+#         phi_omega = np.fft.rfft(phi_t)
+#         return np.fft.irfft(np.abs(np.square(phi_omega)), self.side)
 
 
-    def meff(self, phi):
-        """effective pole mass estimate for t = 1, 2, 3, ... side-1
-            see Equation 28 in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.100.034515"""
-        Gc0 = self.greens_function0(phi)
-        return jnp.arccosh((Gc0[0:-2] + Gc0[2:]) / (2*Gc0[1:-1]))
+#     def meff(self, phi):
+#         """effective pole mass estimate for t = 1, 2, 3, ... side-1
+#             see Equation 28 in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.100.034515"""
+#         Gc0 = self.greens_function0(phi)
+#         return jnp.arccosh((Gc0[0:-2] + Gc0[2:]) / (2*Gc0[1:-1]))
 
-
-class FTtheory:
-    """Latice Phi^4 theory (MCHMC target class) in fourier space"""
-
-    def __init__(self, L, lam, m_sq = -4):
-
-        self.d = L**2
-        self.L = L
-
-
-        K = jnp.asarray(Hypercube(L, 2, 'periodic').Adj)
-
-
-        self.grad_nlogp = jax.value_and_grad(self.nlogp)
-
-        self.transform = lambda phi: jnp.ones(1) * jnp.average(phi)
-
-        self.prior_draw = lambda key: jax.random.normal(key, shape = (self.d, ), dtype = 'float64') #gaussian prior
-
-    def nlogp(self, x):
-        - phi @ K @ phi + lam* jnp.sum(jnp.power(phi, 4))
-
-
-    #Observables
-
-    def susceptibility2(self, phibar):
-        """See appendix in https://arxiv.org/pdf/2207.00283.pdf"""
-        #phibar = jnp.average(phi, axis= 1)
-
-        return self.d * (jnp.average(jnp.square(phibar), axis= -1) - jnp.square(jnp.average(jnp.abs(phibar), axis = -1)))
-
-
-    def susceptibility2_full(self, phibar):
-        """See appendix in https://arxiv.org/pdf/2207.00283.pdf"""
-
-        steps = jnp.arange(1, phibar.shape[-1]+1)
-        phi_1 = jnp.cumsum(jnp.abs(phibar), axis = -1) / steps[None, :]
-        phi_2 = jnp.cumsum(jnp.square(phibar), axis = -1) / steps[None, :]
-
-        return self.d * (phi_2 - jnp.square(phi_1))
-
-    def psd(self, phi):
-        return jnp.square(jnp.abs(jnp.fft.fft2(phi))) / self.L ** 2
-
-    def greens_function0(self, phi):
-        """zero momentum 2-point Green's function, Equations 22 and 23 in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.100.034515"""
-        phi_lattice = np.reshape(phi, (self.side, self.side))
-        phi_t = np.sum(phi_lattice, axis=0)
-        phi_omega = np.fft.rfft(phi_t)
-        return np.fft.irfft(np.abs(np.square(phi_omega)), self.side)
-
-
-    def meff(self, phi):
-        """effective pole mass estimate for t = 1, 2, 3, ... side-1
-            see Equation 28 in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.100.034515"""
-        Gc0 = self.greens_function0(phi)
-        return jnp.arccosh((Gc0[0:-2] + Gc0[2:]) / (2*Gc0[1:-1]))
 
 
 reduced_lam = jnp.linspace(-2.5, 7.5, 16) #lambda range around the critical point (m^2 = -4 is fixed)
@@ -188,38 +83,4 @@ def unreduce_lam(reduced_lam, side):
 
 def reduce_chi(chi, side):
     return chi * np.power(side, -7.0/4.0)
-
-
-
-#
-# ### numpyro ###
-# import numpyro
-# from numpyro.distributions import constraints
-#
-#
-# class phi4_numpyro(numpyro.distributions.Distribution):
-#     """Custom defined phi^4 distribution, see https://forum.pyro.ai/t/creating-a-custom-distribution-in-numpyro/3332/3"""
-#
-#     support = constraints.real_vector
-#
-#     def __init__(self, L, lam):
-#
-#         self.d = L**2
-#         self.L = L
-#         self.lam = lam
-#
-#         self.K = jnp.asarray(Hypercube(L, 2, 'periodic').Adj)
-#
-#         super().__init__(event_shape=(self.d, ))
-#
-#     def sample(self, key, sample_shape=()):
-#         raise NotImplementedError
-#
-#     def log_prob(self, phi):
-#         return phi @ self.K @ phi - self.lam* jnp.sum(jnp.power(phi, 4))
-#
-#
-# def model(L, lam):
-#     phi = numpyro.sample('phi', phi4_numpyro(L, lam))
-
 
