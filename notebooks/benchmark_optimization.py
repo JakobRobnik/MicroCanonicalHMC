4a5a1a3b3dea082a42a4965c8d18cbf3152ceb78
jakob.robnik <jakob.robnik@gmail.com>
2023-07-17 09:50:03 +0200

    refoldering

--- notebooks/benchmark_optimization.py
+++ notebooks/benchmark_optimization.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-
+import sys,os
+home = os.getcwd() + '/../'
+sys.path.append(home)
 from optimization import nesterov
 from optimization.myProblems import *
 
--- notebooks/bias_control.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import jax
-import jax.numpy as jnp
-
-from sampling.sampler import Sampler
-from sampling.benchmark_targets import *
-from ploting.graphs import plot_power_lines
