aa771453d2eca2fb6dda0cb1a7d00e30e0ebc083
= <reubenharry@gmail.com>
2023-10-16 12:10:03 +0200

    remove hamiltonian

--- sampling/dynamics.py
+++ sampling/dynamics.py
@@ -92,22 +92,6 @@ def leapfrog(d, T, V):
 
 
 
-def hamiltonian(integrator, grad_nlogp, d, sequential = True):
-    
-    T = update_position(grad_nlogp)
-    V = update_momentum(d, sequential)
-    
-    if integrator == "LF": #leapfrog (first updates the velocity)
-        return leapfrog(d, T, V)
-
-    elif integrator== 'MN': #minimal norm integrator (first updates the velocity)
-        return minimal_norm(d, T, V)
-      
-    else:
-        raise Exception("Integrator must be either MN (minimal_norm) or LF (leapfrog)")
-
-
-
 def mclmc(hamiltonian_dynamics, partially_refresh_momentum, d):
     
   def step(x, u, g, random_key, L, eps, sigma):
--- sampling/sampler.py
+++ sampling/sampler.py
@@ -268,24 +268,40 @@ class Sampler:
 
         ### sampling ###
 
-        if output == OutputType.normal or output == OutputType.detailed:
-            X, _, E = self.sample_normal(num_steps, x, u, l, g, key, L, eps, sigma, thinning)
-            if output == 'detailed':
-                return X, E, L, eps
-            else:
+        match output:
+            case OutputType.normal:
+                X, _, E = self.sample_normal(num_steps, x, u, l, g, key, L, eps, sigma, thinning)
                 return X
-        elif output == OutputType.expectation:
-            return self.sample_expectation(num_steps, x, u, l, g, key, L, eps, sigma)
+            case OutputType.detailed:
+                X, _, E = self.sample_normal(num_steps, x, u, l, g, key, L, eps, sigma, thinning)
+                return X, E, L, eps
+            case OutputType.expectation:
+                return self.sample_expectation(num_steps, x, u, l, g, key, L, eps, sigma)
+            case OutputType.ess:
+                try:
+                    self.Target.variance
+                except:
+                    raise AttributeError("Target.variance should be defined")
+                return self.sample_ess(num_steps, x, u, l, g, key, L, eps, sigma)
 
-        elif output == OutputType.ess:
-            try:
-                self.Target.variance
-            except:
-                raise AttributeError("Target.variance should be defined")
-            return self.sample_ess(num_steps, x, u, l, g, key, L, eps, sigma)
+        # if output == OutputType.normal or output == OutputType.detailed:
+        #     X, _, E = self.sample_normal(num_steps, x, u, l, g, key, L, eps, sigma, thinning)
+        #     if output == 'detailed':
+        #         return X, E, L, eps
+        #     else:
+        #         return X
+        # elif output == OutputType.expectation:
+        #     return self.sample_expectation(num_steps, x, u, l, g, key, L, eps, sigma)
 
-        else:
-            raise ValueError('output = ' + output + ' is not a valid argument for the Sampler.sample')
+        # elif output == OutputType.ess:
+        #     try:
+        #         self.Target.variance
+        #     except:
+        #         raise AttributeError("Target.variance should be defined")
+        #     return self.sample_ess(num_steps, x, u, l, g, key, L, eps, sigma)
+
+        # else:
+        #     raise ValueError('output = ' + output + ' is not a valid argument for the Sampler.sample')
 
 
     ### for loops which do the sampling steps: ###
