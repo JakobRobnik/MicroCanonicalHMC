fb53300e1b474745c49013c5a3b4a5f07170543d
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2023-12-21 10:53:30 -0800

    general error functions for benchmarking

---
+++ notebooks/ensemble.py
@@ -0,0 +1,126 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import jax
+import jax.numpy as jnp
+import pandas as pd
+
+import blackjax.mcmc.mclmc_ensemble as emclmc
+
+from benchmarks.benchmarks_mchmc import *
+#from benchmarks.german_credit import Target as GermanCredit
+from benchmarks.brownian import Target as Brownian
+from benchmarks.IRT import Target as IRT
+from benchmarks import error
+
+
+#num_cores = jax.local_device_count()
+#print(num_cores, jax.lib.xla_bridge.get_backend().platform)
+
+
+
+def plot_trace(info, bias, target):
+        
+    end_stage1 = lambda: None#plt.plot(steps1 * np.ones(2), plt.gca().get_ylim(), color = 'grey', alpha = 0.2)
+    
+    plt.figure(figsize= (15, 5))
+
+    n1 = info['L'].shape[0]
+    #steps = jnp.concatenate((jnp.arange(n1) * self.n, n1 * self.n + jnp.arange(len(eps)-n1) * self.n * self.grad_evals_per_step))
+    steps = jnp.arange(1, n1+1)
+    
+    
+    ### bias ###
+    plt.subplot(1, 2, 1)
+    plt.title('bias')
+    
+    # true
+    plt.plot(steps, bias[1], color = 'tab:blue', label= 'average')
+    plt.plot(steps, bias[0], color = 'tab:red', label = 'max')
+    
+
+    # equipartition
+    plt.plot(steps, info['equi diag'], color = 'tab:olive', label = 'diagonal equipartition')
+    plt.plot(steps, info['equi full'], '--', color = 'tab:green', label = 'full rank equipartition')
+    
+    plt.plot(steps, jnp.ones(steps.shape) * 1e-2, '-', color = 'black')
+    plt.legend()
+    plt.ylabel(r'$\mathrm{bias}^2$')
+    plt.ylim(1e-4, 1e2)
+    plt.yscale('log')
+    end_stage1()
+    
+    ### stepsize tuning ###
+    plt.subplot(3, 2, 2)
+    plt.plot(steps, info['eevpd observed'], '.', color='tab:blue', alpha = 0.5, label= 'observed')
+    plt.plot(steps, info['eevpd wanted'], '.-', color='purple', label = 'targeted')
+    plt.ylabel("EEVPD")
+    plt.yscale('log')
+    end_stage1()
+    plt.legend()
+    
+    plt.subplot(3, 2, 4)
+    plt.plot(steps, info['stepsize'], '.-', color='royalblue')
+    plt.ylabel(r"$\epsilon$")
+    plt.yscale('log')
+    end_stage1()
+    
+    ### L tuning ###
+    plt.subplot(3, 2, 6)
+    L0 = jnp.sqrt(jnp.sum(target.second_moments))
+    plt.plot(steps, info['L'], '.-', color='tab:orange')
+    plt.plot(steps, L0 * jnp.ones(steps.shape), '-', color='black')
+    end_stage1()
+    plt.ylabel("L")
+    #plt.yscale('log')
+    plt.xlabel('# gradient evaluations')
+    plt.tight_layout()
+    plt.savefig('img/ensemble/adjusted/' + target.name + '.png')
+    plt.close()
+
+
+
+def run(target, num_steps, chains, key):
+    
+    key_sampling, key_init = jax.random.split(key)
+    
+    x_init = jax.vmap(target.prior_draw)(jax.random.split(key_init, chains))
+    
+    info = emclmc.stage1(lambda x: -target.nlogp(x), num_steps, x_init, chains, key_sampling, observables = jnp.square)
+    bias = [error.err(target.second_moments, target.variance_second_moments, contract)(info['expected vals']) for contract in [jnp.max, jnp.average]]
+    
+    grads, success = error.grads_to_low_error(bias[0])
+    
+    plot_trace(info, bias, target)
+    
+    if success:
+       print(grads.astype(int))
+    
+        
+
+def mainn():
+
+
+    targets = [[Banana(prior = 'prior'), 100],
+                [IllConditionedGaussianGamma(prior = 'prior'), 600],
+                #[GermanCredit(), 400],
+                [Brownian(), 500],
+                #[IRT(), 700],
+                [StochasticVolatility(), 2000]]
+
+    chains = 2048
+
+    key = jax.random.PRNGKey(42)
+    
+    for i in [0,]:
+        target, num_steps = targets[i]
+        print(target.name)
+        run(target, num_steps, chains, key)
+
+   
+    
+
+if __name__ == '__main__':
+    
+    mainn()
+
+
--- notebooks/junk.py
+++ notebooks/junk.py
@@ -1,21 +1,6 @@
 import jax
 import jax.numpy as jnp
-import numpy as np
-import matplotlib.pyplot as plt
+import blackjax.mcmc.mclmc_ensemble as emclmc
 
-from scipy.special import lambertw
-
-from scipy.integrate import odeint
-
-from mclmc.sampler import Sampler
-from benchmarks.benchmarks_mchmc import *
-
-
-target = IllConditionedGaussian(d = 100, condition_number= 1000.)
-
-sampler = Sampler(target, diagonal_preconditioning= True, adjust = False)
-
-x = sampler.sample(10000)
-print(sampler.hyp)
-print(sampler.hyp.sigma / jnp.sqrt(target.second_moments))
+jax.random.PRNGKey(42)
 
