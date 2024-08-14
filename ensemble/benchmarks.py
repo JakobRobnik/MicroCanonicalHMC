c48f5ec15cdf938066a0e194349bc19917b75ca5
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2024-08-14 04:13:44 -0700

    ground truth cov and moments

--- ensemble/benchmarks.py
+++ ensemble/benchmarks.py
@@ -5,13 +5,14 @@ import os
 os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=128'
 
 from blackjax.adaptation.ensemble_mclmc import algorithm as emclmc
+from blackjax.adaptation.ensemble_umclmc import Parallelization
 from benchmarks.inference_models import *
 
 num_cores = jax.local_device_count()
 print(num_cores, jax.lib.xla_bridge.get_backend().platform)
 
 
-def plot_trace(info1, info2, target, mclachlan):
+def plot_trace(info1, info2, model, mclachlan):
         
     grads_per_step = 2 if mclachlan else 1 # in stage 2
     
@@ -84,7 +85,7 @@ def plot_trace(info1, info2, target, mclachlan):
     
     ### L tuning ###
     plt.subplot(3, 2, 6)
-    L0 = jnp.sqrt(jnp.sum(target.second_moments))
+    L0 = jnp.sqrt(jnp.sum(model.E_x2))
     plt.plot(steps1, info1['L'], '.-', color='chocolate')
 
     plt.plot(steps2, info2['steps_per_sample'] * info2['stepsize'], '.-', color='chocolate')
@@ -96,38 +97,32 @@ def plot_trace(info1, info2, target, mclachlan):
     
     
     plt.tight_layout()
-    plt.savefig('img/ensemble/adjusted/' + target.name + '.png')
+    plt.savefig('img/' + model.name + '.png')
     plt.close()
 
 
 
-
-def run(target, num_steps, chains, key):
+def run(model, num_steps, num_chains, key):
     
     mclachlan = True
     key_sampling, key_init = jax.random.split(key)
-    
-    x_init = jax.vmap(target.prior_draw)(jax.random.split(key_init, chains))
-    transform = jax.vmap(target.transform)
+    parallelization = Parallelization(pmap_chains= num_cores, vmap_chains= num_chains // num_cores)
+    initial_position = parallelization.pvmap(model.sample_init)(jax.random.split(key_init, num_chains).reshape(parallelization.shape))
+    transform = parallelization.pvmap(model.transform)
     
     def observables(x):
         f = jnp.average(jnp.square(transform(x)), axis = 0)
-        bsq = jnp.square(f - target.second_moments) / target.variance_second_moments    
+        bsq = jnp.square(f - model.E_x2) / model.Var_x2
         return jnp.array([jnp.max(bsq), jnp.average(bsq)])
     
-    info1, info2 = emclmc(lambda x: -target.nlogp(x), num_steps, chains, 
-                          x_init, key_sampling,
-                          30,
-                          mclachlan, 
-                          observables=observables)
+    info1, info2 = emclmc(model.logdensity_fn, num_steps, parallelization, 
+                          initial_position, key_sampling,
+                          num_steps_per_sample= 3,
+                          mclachlan= mclachlan, 
+                          observables= observables)
     
-    #grads, success = error.grads_to_low_error(bias[0])
+    plot_trace(info1, info2, model, mclachlan)
     
-    plot_trace(info1, info2, target, mclachlan)
-    
-    # if success:
-    #    print(grads.astype(int))
-
 
 
 def mainn():
@@ -139,7 +134,7 @@ def mainn():
                 [ItemResponseTheory(), 1000],
                 [StochasticVolatility(), 2000]]
 
-    chains = 4096
+    chains = 512
 
     key = jax.random.key(42)
     
