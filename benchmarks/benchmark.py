b30cbdd96727e0a5b5d94f51a4d53ad1332633dc
= <reubenharry@gmail.com>
2024-10-23 18:15:28 -0400

    we

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -450,7 +450,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
                             ess_corr.mean().item(),
                             ess_corr.min().item(),
                             num_steps,
-                            False
+                            True
                         )
                     ] = ess
 
@@ -477,7 +477,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
                     
                     results[
                         (
-                            model.name, model.ndims, "mclmc", params.L.mean().item(), params.step_size.mean().item(), (integrator_type), "standard", 1.0, preconditioning, 0, ess_avg, ess_corr.mean().item(), ess_corr.min().item(), num_steps, False
+                            model.name, model.ndims, "mclmc", params.L.mean().item(), params.step_size.mean().item(), (integrator_type), "standard", 1.0, preconditioning, 0, ess_avg, ess_corr.mean().item(), ess_corr.min().item(), 1/(1/ess_corr).mean().item(), num_steps, False
                         )
                     ] = ess
                     print(f"unadjusted mclmc with tuning, grads to low bias avg {grads_to_low_avg}")
@@ -510,7 +510,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
                                 1 / L_proposal_factor,
                                 ess_avg,
                                 ess_corr.mean().item(),
-                                ess_corr.min().item(),
+                                ess_corr.min().item(), 1/(1/ess_corr).mean().item(),
                                 num_steps,
                                 False
                             )
@@ -539,7 +539,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
                                 1 / L_proposal_factor,
                                 ess_avg,
                                 ess_corr.mean().item(),
-                                ess_corr.min().item(),
+                                ess_corr.min().item(), 1/(1/ess_corr).mean().item(),
                                 num_steps,
                                 True
                             )
@@ -570,7 +570,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
                                 1 / L_proposal_factor,
                                 ess_avg,
                                 ess_corr.mean().item(),
-                                ess_corr.min().item(),
+                                ess_corr.min().item(), 1/(1/ess_corr).mean().item(),
                                 num_steps,
                                 False
                             )
@@ -603,7 +603,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
                         0,
                         ess_avg,
                         ess_corr.mean().item(),
-                        ess_corr.min().item(),
+                        ess_corr.min().item(), 1/(1/ess_corr).mean().item(),
                         num_steps,
                         None
                     )
@@ -611,7 +611,7 @@ def benchmark_adjusted_mclmc(batch_size, key_index=1):
 
         df = pd.Series(results).reset_index()
         df.columns = [
-            "model", "dims", "sampler", "L", "step_size", "integrator", "tuning", "acc_rate", "preconditioning", "inv_L_prop", "ess_avg", "ess_corr_avg", "ess_corr_min", "num_steps", "worst", "ESS"]
+            "model", "dims", "sampler", "L", "step_size", "integrator", "tuning", "acc_rate", "preconditioning", "inv_L_prop", "ess_avg", "ess_corr_avg", "ess_corr_min", "ess_corr_inv_mean" "num_steps", "worst", "ESS"]
         # df.result = df.result.apply(lambda x: x[0].item())
         # df.model = df.model.apply(lambda x: x[1])
         df.to_csv(f"results{model.name}{model.ndims}{key_index}.csv", index=False)
